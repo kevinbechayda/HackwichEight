ed8bd54f14875f268d9ea83d0f62f374be2075d1
Kevin Bechayda <kevinbechayda@gmail.com>
2018-04-02 10:27:50 -1000

    Completed HackwichEight

---
+++ HackwichEight/Restaurant.swift
@@ -0,0 +1,34 @@
+//
+//  Restaurant.swift
+//  HackwichEight
+//
+//  Created by Kevin Bechayda on 4/2/18.
+//  Copyright © 2018 Kevin Bechayda. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Restaurant: NSObject, MKAnnotation {
+
+    //create 3 variables hold data for the annotation
+    let restaurantTitle:String?
+    let restaurantType: String
+    let coordinate: CLLocationCoordinate2D
+    
+//initialize the MKAnnotation object
+    init(title: String, type: String, coordinate: CLLocationCoordinate2D) {
+        self.restaurantTitle = title
+        self.restaurantType = type
+        self.coordinate = coordinate
+        
+        super.init()
+        
+    }
+    
+    var subtitle: String? {
+        return restaurantTitle
+        
+    }
+    
+}
